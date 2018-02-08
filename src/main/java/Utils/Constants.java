9a6445f56e5ec395f774b3c048328d6be17da2d8
Hafiz Jefri <hafiz.jefri@gmail.com>
2018-02-08 15:30:14 +0800

    Update constants
    Signed-off-by: Hafiz Jefri <hafiz.jefri@gmail.com>

--- src/main/java/Utils/Constants.java
+++ src/main/java/Utils/Constants.java
@@ -9,6 +9,8 @@ public class Constants {
         ERROR
     }
 
+    public static final String VERSES_DIR = "output/tashih/processed/verse";
+    public static final String OVERLAP_DIR = "output/tashih/processed/line/actual";
     public static final String UPLOAD_DIR = "output/uploads";
 
     public static File uploadDir = new File(Constants.UPLOAD_DIR);
