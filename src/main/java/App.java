5dd185197555fbb645a7831415f84eaacdeef083
Hafiz Jefri <hafiz.jefri@gmail.com>
2018-05-02 03:06:49 +0800

    - Implement Report controller

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import Utils.Constants;
 import frontEnd.apiController.ProcessController;
+import frontEnd.apiController.ReportController;
 import frontEnd.apiController.UserController;
+import frontEnd.apiService.ReportService;
 import frontEnd.apiService.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +22,7 @@ public class App implements SparkApplication {
         uploadDir.mkdirs();
         new UserController(new UserService());
         new ProcessController(new UserService());
+        new ReportController(new ReportService());
     }
 
 	@Override
